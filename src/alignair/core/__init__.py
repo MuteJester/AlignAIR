34d4aaba4efc2912cd6b527b719e6ca250fd9ea3
thomas <thomaskon90@gmail.com>
2026-06-16 15:42:40 +0300

    feat(alignair): segment extraction + germline_coords; export DNAlignAIR

--- src/alignair/core/__init__.py
+++ src/alignair/core/__init__.py
@@ -1,5 +1,7 @@
 from .single_chain import SingleChainAlignAIR
 from .multi_chain import MultiChainAlignAIR
 from .output import AlignAIROutput
+from .dnalignair import DNAlignAIR, extract_segment
 
-__all__ = ["SingleChainAlignAIR", "MultiChainAlignAIR", "AlignAIROutput"]
+__all__ = ["SingleChainAlignAIR", "MultiChainAlignAIR", "AlignAIROutput",
+           "DNAlignAIR", "extract_segment"]
