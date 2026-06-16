711839d4d8a538a0cb96c98aba113f0c3be6ffbf
thomas <thomaskon90@gmail.com>
2026-06-16 09:18:10 +0300

    feat(alignair): export core models from package

---
+++ src/alignair/core/__init__.py
@@ -0,0 +1,5 @@
+from .single_chain import SingleChainAlignAIR
+from .multi_chain import MultiChainAlignAIR
+from .output import AlignAIROutput
+
+__all__ = ["SingleChainAlignAIR", "MultiChainAlignAIR", "AlignAIROutput"]
