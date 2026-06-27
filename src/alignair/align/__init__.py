85ab927b4fc9c263a4346837afa03b6749e89308
thomas <thomaskon90@gmail.com>
2026-06-27 18:04:27 +0300

    align: non-learned k-mer seed prefilter (swap-robust candidate admission)

--- src/alignair/align/__init__.py
+++ src/alignair/align/__init__.py
@@ -1,4 +1,5 @@
 from .backend import AlignResult, Aligner, get_aligner
 from .parasail import ParasailAligner
+from .seed_prefilter import SeedPrefilter
 
-__all__ = ["AlignResult", "Aligner", "get_aligner", "ParasailAligner"]
+__all__ = ["AlignResult", "Aligner", "get_aligner", "ParasailAligner", "SeedPrefilter"]
