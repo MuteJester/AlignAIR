c1f2dcf1e666d230bb84248d44f5e99711c711ca
thomas <thomaskon90@gmail.com>
2026-07-07 12:15:54 +0300

    chore: park airristotle to .private; focus on faithful AlignAIR recreation

--- src/alignair/airristotle/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tokenizer import AIRRTokenizer
-from .config import AIRRConfig
-from .model import AIRRistotle, airristotle_loss
-from .infer import align, called_names
-
-__all__ = ["AIRRTokenizer", "AIRRConfig", "AIRRistotle", "airristotle_loss", "align", "called_names"]
